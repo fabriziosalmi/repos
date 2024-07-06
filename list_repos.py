a732e10ee1b00b187a07b5938e2026d1fdc0b603
fab <fabrizio.salmi@gmail.com>
2024-07-06 19:54:16 +0200

    Update list_repos.py

--- list_repos.py
+++ list_repos.py
@@ -58,24 +58,24 @@ def humanize_commit_date(commit_date):
     elif diff.days == 1:
         return 'yesterday'
     elif diff.days < 7:
-        return f'{diff.days} days ago'
+        return f'{diff.days} days'
     elif diff.days < 30:
         weeks = diff.days // 7
-        return f'{weeks} {"week" if weeks == 1 else "weeks"} ago'
+        return f'{weeks} {"week" if weeks == 1 else "weeks"}'
     elif diff.days < 365:
         months = diff.days // 30
-        return f'{months} {"month" if months == 1 else "months"} ago'
+        return f'{months} {"month" if months == 1 else "months"}'
     else:
         years = diff.days // 365
-        return f'{years} {"year" if years == 1 else "years"} ago'
+        return f'{years} {"year" if years == 1 else "years"}'
 
 def get_freshness_badge(humanized_commit_date, for_markdown=False):
     color = 'lightgrey'
-    if 'today' in humanized_commit_date or 'yesterday' in humanized_commit_date or 'days ago' in humanized_commit_date:
+    if 'today' in humanized_commit_date or 'yesterday' in humanized_commit_date or 'days' in humanized_commit_date:
         color = 'brightgreen'
-    elif 'weeks ago' in humanized_commit_date or 'week ago' in humanized_commit_date or 'month ago' in humanized_commit_date or 'months ago' in humanized_commit_date and '1 month' not in humanized_commit_date:
+    elif 'weeks ago' in humanized_commit_date or 'week' in humanized_commit_date or 'month' in humanized_commit_date or 'months' in humanized_commit_date and '1 month' not in humanized_commit_date:
         color = 'yellow'
-    elif 'years ago' in humanized_commit_date or 'year ago' in humanized_commit_date or '1 month' in humanized_commit_date:
+    elif 'years ago' in humanized_commit_date or 'year' in humanized_commit_date or 'months' in humanized_commit_date:
         color = 'red'
 
     if for_markdown:
