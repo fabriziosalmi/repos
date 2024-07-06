54830727a3eb87d78a161555244de2285c385880
fab <fabrizio.salmi@gmail.com>
2024-07-06 17:37:36 +0200

    Update list_repos.py

--- list_repos.py
+++ list_repos.py
@@ -213,7 +213,7 @@ def generate_html_page(repositories):
                     <img src="https://img.shields.io/github/last-commit/{GITHUB_USERNAME}/{repo_name}?style=flat-square" alt="Last Commit"> 
                 </td>
                 <td>
-                    <img src="https://img.shields.io/github/⭐️/{GITHUB_USERNAME}/{repo_name}?style=flat-square" alt="Stars"> 
+                    <img src="https://img.shields.io/github/stars/{GITHUB_USERNAME}/{repo_name}?style=flat-square" alt="Stars"> 
                 </td>
             </tr>
 '''
