94279b92c575ea8517bd7820cbadab4e03b6fc7b
fabriziosalmi <fabrizio.salmi@gmail.com>
2025-11-28 22:25:52 +0100

    Deploy: Update Tailwind integration and adjust dependencies in configuration

--- astro-frontend/astro.config.mjs
+++ astro-frontend/astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwindcss';
+import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 // https://astro.build/config
